18ceedeaaea5821d74882451d734819eab63431a
JamieFlea <98387467+JamieFlea@users.noreply.github.com>
2022-08-08 14:10:30 +0100

    Update eightBall.js

--- eightBall.js
+++ eightBall.js
@@ -1,35 +1,32 @@
 let userName = 'Jamie';
 userName ? console.log(`Hello, ${userName}!`) : console.log('Hello!'); 
 const userQuestion = 'Will I become a werewolf tonight?';
-console.log (`The user asked: ${userQuestion}`);
+console.log (`The user ${userName} asked: ${userQuestion}`);
 const randomNumber = Math.floor(Math.random() * 8);
 let eightBall = '';
 switch (randomNumber){
   case 0:
   eightBall = 'Whadda you think?';
   break
-  case 1:
-  eightBall = 'It is certain';
-  break
-  case 2: 
+  case 1: 
   eightBall = 'It is decidedly so';
   break
-  case 3:
+  case 2:
   eightBall = 'Reply hazy try again';
   break
-  case 4:
+  case 3:
   eightBall = 'Cannot predict now';
   break
-  case 5: 
+  case 4: 
   eightBall = 'Do not count on it';
   break
-  case 6: 
+  case 5: 
   eightBall = 'My sources say no';
   break
-  case 7:
+  case 6:
   eightBall = 'Outlook not so good';
   break
-  case 8:
+  case 7:
   eightBall = 'Signs point to yes';
   break;
   }
